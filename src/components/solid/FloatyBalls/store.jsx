b9c0ba18f7c53c3a47ae8c916ca9d0312926f573
Daniel Rötzer <52409858+danielroetzer@users.noreply.github.com>
2023-01-06 23:26:56 +0100

    remove unused import

--- src/components/solid/FloatyBalls/store.jsx
+++ src/components/solid/FloatyBalls/store.jsx
@@ -1,5 +1,4 @@
 import { createStore } from 'solid-js/store';
-import { isServer } from "solid-js/web";
 import { randomFloat, randomHexColor, randomInt } from '@utils/math';
 
 export const [floatyBalls, setFloatyBalls] = createStore({
