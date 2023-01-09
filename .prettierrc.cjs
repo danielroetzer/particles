1ff9b250f37848b9cc8aa5f4b66e99ce7d1b3756
Daniel Rötzer <52409858+danielroetzer@users.noreply.github.com>
2023-01-09 22:08:36 +0100

    add svelte plugin for prettier

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,10 +2,13 @@ module.exports = {
     singleQuote: true,
     arrowParens: 'avoid',
 
-    // https://github.com/withastro/prettier-plugin-astro#pnpm-support-1
-    plugins: [require.resolve('prettier-plugin-astro')],
+    plugins: [
+        require.resolve('prettier-plugin-astro'),
+        require.resolve('prettier-plugin-svelte'),
+    ],
     overrides: [
         {
+            // https://github.com/withastro/prettier-plugin-astro#pnpm-support-1
             files: '*.astro',
             options: {
                 parser: 'astro',
