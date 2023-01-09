9c7c0cfb9423002cb72411840255ef999a943a3f
Daniel Rötzer <52409858+danielroetzer@users.noreply.github.com>
2023-01-09 12:51:43 +0100

    `pnpm astro add svelte`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danielroetzer.github.io',
   base: '/particles',
-  integrations: [solidJs(), sitemap()]
+  integrations: [solidJs(), svelte(), sitemap()]
 });
