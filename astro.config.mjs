b464b8920249b615bd96f8d857992ecd48d0f05d
Daniel Rötzer <52409858+danielroetzer@users.noreply.github.com>
2023-02-10 19:58:17 +0100

    add `@astro/prefetch`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://danielroetzer.github.io',
     base: '/particles',
-    integrations: [solidJs(), svelte(), sitemap()],
+    integrations: [solidJs(), svelte(), sitemap(), prefetch()],
 });
